82826ae9dc8bcd4545d3286b559327e4fbbe7ff7
k4neda <urban-trendsport@hotmail.de>
2015-04-29 12:59:45 +0200

    added fancy curl for url redirects

--- crawler.sh
+++ crawler.sh
@@ -10,5 +10,7 @@ echo "crawling for country $CC"
 
 for i in `seq 0 10`;
 do
-	curl --silent http://www.alexa.com/topsites/countries\;$i/$CC | grep -oP '(?<=\<a href="\/siteinfo\/).*?(?=">)'
+	curl --silent http://www.alexa.com/topsites/countries\;$i/$CC | grep -oP '(?<=\<a href="\/siteinfo\/).*?(?=">)' | while read line; do curl -Ls -o /dev/null -w %{url_effective} $line; echo; done
+	#while read line; do echo "$line"; done
+	#curl -Ls -o /dev/null -w %{url_effective} #URL here 
 done 
